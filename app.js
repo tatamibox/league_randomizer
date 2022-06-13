cece80bee31028468653470207ebcac4057ba086
tatamibox <tat4mi@gmail.com>
2022-06-12 20:07:15 -0700

    added functionality for other roles

--- app.js
+++ app.js
@@ -3,17 +3,33 @@
 const amumu = {
     champName: 'Amumu',
     position: 'Jungle',
-    buildsLink: 'https://u.gg/lol/champions/amumu/build',
-    imageLink: 'imgs/champions/amumu_LoT.jpg'
+    buildsLink: 'https://u.gg/lol/champions/jungle/amumu/build',
+    imageLink: 'imgs/champions/jungle/amumu_LoT.jpg'
 }
 
 const leeSin = {
     champName: 'Lee Sin',
     position: 'Jungle',
-    buildsLink: 'https://u.gg/lol/champions/leesin/build',
-    imageLink: 'imgs/champions/leesin_LoT.jpg'
+    buildsLink: 'https://u.gg/lol/champions/jungle/leesin/build',
+    imageLink: 'imgs/champions/jungle/leesin_LoT.jpg'
 }
 const jungle = [amumu, leeSin];
+
+const aphelios = {
+    champName: 'Aphelios',
+    position: 'ADC',
+    buildsLink: 'https://u.gg/lol/champions/aphelios/build',
+    imageLink: 'imgs/champions/adc/aphelios_LoT.jpg'
+}
+
+const ashe = {
+    champName: 'Ashe',
+    position: 'ADC',
+    buildsLink: 'https://u.gg/lol/champions/ashe/build',
+    imageLink: 'imgs/champions/adc/ashe_LoT.jpg'
+}
+
+const adc = [aphelios, ashe];
 
 // jungle objects finish
 
@@ -25,7 +41,7 @@ const jungle = [amumu, leeSin];
 //
 
 
-
+const cardRole = document.querySelector('.cardRole');
 const roleSelect = document.querySelector('.roleSelect');
 const randomizeButton = document.querySelector('.randomActivate');
 let currentRole;
@@ -49,13 +65,23 @@ function changeChampImg() {
 function changeChampName() {
     const currentChampName = document.querySelector('.cardName');
     currentChampName.innerHTML = `${currentRole[currentChamp].champName}`;
-}
+};
 //
 
 // changes the U.GG link under champ info
 function changeChampBuildLink() {
     const currentBuildLink = document.querySelector('.buildURL');
     currentBuildLink.href = `${currentRole[currentChamp].buildsLink}`;
+};
+//
+
+// changes the role display on the card of the randomizer
+function changeCardRoleDisplay() {
+    if (currentRole === jungle) {
+        cardRole.innerHTML = 'Jungle';
+    } else if (currentRole === adc) {
+        cardRole.innerHTML = 'ADC';
+    }
 }
 
 
@@ -64,26 +90,28 @@ function randomizeAll() {
     if (roleSelect.value === 'jungle') {
         currentRole = jungle;
     } else if (roleSelect.value === 'adc') {
-        alert('work in progress for this role');
-        return;
+        currentRole = adc;
     } else if (roleSelect.value === 'mid') {
         alert('work in progress for this role');
-        return;
+
     } else if (roleSelect.value === 'top') {
         alert('work in progress for this role');
-        return;
+
     } else if (roleSelect.value === 'support') {
         alert('work in progress for this role');
-        return;
+
     }
-    else {
-        alert('No role is selected, so please select a role.');
-        return;
-    }
+    else if (roleSelect.value === 'allPositions') {
+        alert('work in progress for this role');
+    } else if (roleSelect.value === 'noRoleSelected') {
+        alert('No role is currently selected.')
+    };
+
     randomizeChampionSelected();
     changeChampImg();
     changeChampName();
     changeChampBuildLink();
+    changeCardRoleDisplay();
 
 
 }
